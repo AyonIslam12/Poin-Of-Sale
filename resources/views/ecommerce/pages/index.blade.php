0de5801859ab8e08758cb5c9755c6628721df352
Md.Mehedi Hasan <61658463+AyonIslam12@users.noreply.github.com>
2021-07-03 12:48:56 +0600

    done for this project

--- resources/views/ecommerce/pages/index.blade.php
+++ resources/views/ecommerce/pages/index.blade.php
@@ -12,10 +12,7 @@ Point-Of-Sale
                 <div class="row">
                     <div class="col-md-12">
                         <div class="row">
-                            <div class="col-md-4">
-                                <h3 class="text-uppercase">Pos</h3>
-                            </div>
-                            <div class="col-md-8">
+                            <div class="col-md-6">
                                 <input
                                 @keyup="search_product($event.target.value)"
                                 type="text"
